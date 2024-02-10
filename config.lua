3b799303c56798ac6333f7e403fc9a5615fce257
Bailey Kruszewski <bkrusz@icloud.com>
2024-02-10 11:45:46 -0700

    Fixed indentation issue with .md files

--- config.lua
+++ config.lua
@@ -5,5 +5,18 @@ reload('user.keymaps')
 vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.breakat = [[\ \ ;:,!?]]
-vim.opt.conceallevel = 1
 vim.wo.relativenumber = true
+
+-- Terrible workaround to get .md files to use two space tabs
+-- I forget why this was a problem in the first place
+vim.api.nvim_create_augroup("lvim_user", {})
+lvim.autocommands = {
+  {
+    { "BufEnter", "BufWinEnter" },
+    {
+      group = "lvim_user",
+      pattern = "*.md",
+      command = "setlocal ts=2 sw=2",
+    },
+  },
+}
